5c55b2967a9a60065672e7afde4ebf8ee4cbb802
Justin Yoo <justin.yoojh@gmail.com>
2018-04-19 23:03:09 +1000

    Add Cosmos DB Trigger

--- src/V1.FunctionApp/TestTimerTrigger.cs
+++ src/V1.FunctionApp/TestTimerTrigger.cs
@@ -1,15 +1,15 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+//using System;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Host;
 
-namespace V1.FunctionApp
-{
-    public static class TestTimerTrigger
-    {
-        [FunctionName("TestTimerTrigger")]
-        public static void Run([TimerTrigger("0/10 * * * * *")]TimerInfo myTimer, TraceWriter log)
-        {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-        }
-    }
-}
+//namespace V1.FunctionApp
+//{
+//    public static class TestTimerTrigger
+//    {
+//        [FunctionName("TestTimerTrigger")]
+//        public static void Run([TimerTrigger("0/10 * * * * *")]TimerInfo myTimer, TraceWriter log)
+//        {
+//            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+//        }
+//    }
+//}
