e42fee96f341a2d39d654a89be8ec6582939f53e
Justin Yoo <justin.yoojh@gmail.com>
2018-04-19 21:25:33 +1000

    Add Timer triggers

---
+++ src/V1.FunctionApp/TestTimerTrigger.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace V1.FunctionApp
+{
+    public static class TestTimerTrigger
+    {
+        [FunctionName("TestTimerTrigger")]
+        public static void Run([TimerTrigger("0/10 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
